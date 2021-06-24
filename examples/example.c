0344c8e36f5c02c887874af50a36571151e78c06
Julian <julian.blazek@outlook.de>
2021-06-24 21:20:11 +0200

    Add Example

---
+++ examples/example.c
@@ -0,0 +1,9 @@
+#include<ESP8266_multipart.h>
+
+ESP8266_multipart multipart("myexampleserver.com"); // Sets the host to myexampleserver.com and the port to 80
+multipart.setPort(8772); // Sets the port to 8772
+int status = multipart.sendFile("/path/to/send/data/to", fileObject);
+/**
+ * status == 0 -> couldn't connect to server
+ * status != 0 -> HTTP Status Code returned by server
+ */
